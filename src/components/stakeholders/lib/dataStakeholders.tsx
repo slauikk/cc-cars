e2fc067ba17865041893c43659aad0bad0442832
slauikk <zzzyaroslav63@gmail.com>
2025-02-07 14:57:39 +0200

    feat: add content

--- src/components/stakeholders/lib/dataStakeholders.tsx
+++ src/components/stakeholders/lib/dataStakeholders.tsx
@@ -1,81 +1,83 @@
-import Logo_1 from "@/assets/stakeholders/Вікі-тур.jpg"
-import Logo_2 from "@/assets/stakeholders/Глобус.jpg"
-import Logo_3 from "@/assets/stakeholders/Рудісон.jpg"
-import Logo_4 from "@/assets/stakeholders/Рід-тур.jpg"
-import Logo_5 from "@/assets/stakeholders/ТІЦ.jpg"
-import Logo_6 from "@/assets/stakeholders/пасіка.jpg"
+import Logo_1 from "@/assets/stakeholders/vatra.png"
+import Logo_2 from "@/assets/stakeholders/schreder.jpeg"
+import Logo_3 from "@/assets/stakeholders/carinex.png"
+import Logo_4 from "@/assets/stakeholders/biol.png"
+import Logo_5 from "@/assets/stakeholders/123.gif"
+
 
 export const dataStakeholders = [
   {
     logo: Logo_1,
-    title: "Туристичний оператор Вікі-тур",
+    title: "Корпорація ВАТРА",
     info: [
-      "м. Тернопіль, вул. Гоголя, буд. 5, офіс 31",
-      "+38 (067) 352-0276",
-      <a href="https://viki-tour.com.ua/" target="_blank">
-        https://viki-tour.com.ua/
+      "Тернопіль, вулиця Микулинецька, 46",
+      "+380 (352) 52-31-37",
+      <a href="https://vatra.ua/ukr/" target="_blank">
+        https://vatra.ua/ukr/
       </a>,
     ],
   },
   {
     logo: Logo_2,
-    title: "Готельний комплекс «Глобус»",
+    title: "ТОВ \"Шредер\"",
     info: [
-      "м. Тернопіль, вул. Степана Будного, 18",
-      "+38 (068) 646-999-5,  +38 (066) 086-00-69",
-      <a href="https://www.hotel-globus.com/" target="_blank">
-        https://www.hotel-globus.com/
+      "Тернопіль, вулиця Микулинецька, 46-Б",
+      "+38 (080) 050-569-9" ,
+      <a href="https://ua.schreder.com/uk" target="_blank">
+        https://ua.schreder.com/uk
       </a>,
     ],
   },
   {
     logo: Logo_3,
-    title: "Rudison Hotel & Restaurant",
+    title: "Carinex",
     info: [
-      <>Галицька вулиця, 44а, Великі Гаї, Тернопільська область</>,
-      <>+38 (068) 800 3500</>,
-      <a href="https://rudison.te.ua/" target="_blank">
-        https://rudison.te.ua/
+      <>Тернопіль, вулиця Василя Симоненка, 6</>,
+      <>
+
+        +38 (068) 169-48-12, +38 (096) 763-68-07
+      </>,
+      <a href="https://carinex.ua" target="_blank">
+        https://carinex.ua
       </a>,
     ],
   },
   {
     logo: Logo_4,
-    title: "Туристичний оператор Рід-тур",
+    title: "Biol",
     info: [
-      <>Тернопіль, просп. Степана Бандери 45</>,
-      <>(0352) 51-50-50 +38 (096) 827-53-74</>,
-      <a href="https://www.facebook.com/ridtur/?locale=uk_UA" target="_blank">
-        https://www.facebook.com/ridtur/?locale=uk_UA
+      <>+38(067) 610 31 30</>,
+      <a href="https://biol.com.ua" target="_blank">
+        https://biol.com.ua
       </a>,
-      <a href="https://www.instagram.com/ridtur/" target="_blank">
-        https://www.instagram.com/ridtur/
-      </a>,
+
     ],
   },
   {
     logo: Logo_5,
-    title: "Туристично-інформаційний центр м. Тернопіль",
+    title: "АвтоМафія",
     info: [
-      "вулиця Михайла, вулиця Грушевського, 2, Тернопіль",
-      "+38 (098) 052-0720",
-      <a
-        href="https://www.facebook.com/ticternopil/?locale=ru_RU"
-        target="_blank"
-      >
-        https://www.facebook.com/ticternopil/?locale=ru_RU
-      </a>,
-      <a href="https://www.instagram.com/ternopil_tic/" target="_blank">
-        https://www.instagram.com/ternopil_tic/
-      </a>,
+      "надайте інформацію",
+      "",
+
     ],
   },
   {
-    logo: Logo_6,
-    title: "Приватне підприємство «АГРОН»",
+    logo: Logo_5,
+    title: "Автопалац Тернопіль",
     info: [
-      "вул. Промислова, 1, с. Товстолуг, Тернопільський р-н, Тернопільська обл.",
-      "(0352) 293 625",
+      "надайте інформацію",
+      "",
+
+    ],
+  },
+  {
+    logo: Logo_5,
+    title: "КАМАЗ Центр",
+    info: [
+      "надайте інформацію",
+      "",
+
     ],
   },
 ]
