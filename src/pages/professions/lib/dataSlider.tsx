be261457efc44b3d9f79de5c66d4cb07b3649d5b
slauikk <zzzyaroslav63@gmail.com>
2025-02-18 18:47:11 +0200

    feat: add content

--- src/pages/professions/lib/dataSlider.tsx
+++ src/pages/professions/lib/dataSlider.tsx
@@ -4,33 +4,61 @@ import {getSliderElements} from "@/utils/getSliderElements"
 
 //b
 export const dataSponsorsSource_b: ISponsors[] = [
-  {img: "/assets/images/professions/бджоляр/1.jpg"},
-  {img: "/assets/images/professions/бджоляр/2.jpg"},
-  {img: "/assets/images/professions/бджоляр/3.jpg"},
+  {img: "/assets/images/professions/1/1.jpg"},
+  {img: "/assets/images/professions/1/2.jpg"},
+  {img: "/assets/images/professions/1/3.jpg"},
+  {img: "/assets/images/professions/1/4.jpg"},
 ]
 export const dataSponsors_b = getSliderElements(dataSponsorsSource_b)
 
 //h-1
 export const dataSponsorsSource_h_1: ISponsors[] = [
-  {img: "/assets/images/professions/готель/1.jpg"},
-  {img: "/assets/images/professions/готель/2.jpg"},
-  {img: "/assets/images/professions/готель/3.jpg"},
+  {img: "/assets/images/professions/2/1.jpg"},
+  {img: "/assets/images/professions/2/2.jpg"},
+  {img: "/assets/images/professions/2/3.jpg"},
+  {img: "/assets/images/professions/2/5.jpg"},
+  {img: "/assets/images/professions/2/6.jpg"},
+  {img: "/assets/images/professions/2/7.jpg"},
+  {img: "/assets/images/professions/2/8.jpg"},
+  {img: "/assets/images/professions/2/9.jpg"},
+  {img: "/assets/images/professions/2/10.jpg"},
+  {img: "/assets/images/professions/2/11.jpg"},
+  {img: "/assets/images/professions/2/12.jpg"},
+  {img: "/assets/images/professions/2/13.jpg"},
+  {img: "/assets/images/professions/2/14.jpg"},
 ]
 export const dataSponsors_h_1 = getSliderElements(dataSponsorsSource_h_1)
 
 //h-2
 export const dataSponsorsSource_h_2: ISponsors[] = [
-  {img: "/assets/images/professions/готель/4.jpg"},
-  {img: "/assets/images/professions/готель/5.jpg"},
-  {img: "/assets/images/professions/готель/6.jpg"},
+  {img: "/assets/images/professions/3/1.jpg"},
+  {img: "/assets/images/professions/3/2.jpg"},
+  {img: "/assets/images/professions/3/3.jpg"},
+  {img: "/assets/images/professions/3/5.jpg"},
+  {img: "/assets/images/professions/3/6.jpg"},
+  {img: "/assets/images/professions/3/7.jpg"},
+  {img: "/assets/images/professions/3/8.jpg"},
+  {img: "/assets/images/professions/3/9.jpg"},
+  {img: "/assets/images/professions/3/10.jpg"},
+  {img: "/assets/images/professions/3/11.jpg"},
+  {img: "/assets/images/professions/3/12.jpg"},
 ]
+
 export const dataSponsors_h_2 = getSliderElements(dataSponsorsSource_h_2)
 
 //h-3
 export const dataSponsorsSource_h_3: ISponsors[] = [
-  {img: "/assets/images/professions/готель/7.jpg"},
-  {img: "/assets/images/professions/готель/8.jpg"},
-  {img: "/assets/images/professions/готель/9.jpg"},
+  {img: "/assets/images/professions/4/1.jpg"},
+  {img: "/assets/images/professions/4/2.jpg"},
+  {img: "/assets/images/professions/4/3.jpg"},
+  {img: "/assets/images/professions/4/5.jpg"},
+  {img: "/assets/images/professions/4/6.jpg"},
+  {img: "/assets/images/professions/4/7.jpg"},
+  {img: "/assets/images/professions/4/8.jpg"},
+  {img: "/assets/images/professions/4/9.jpg"},
+  {img: "/assets/images/professions/4/10.jpg"},
+  {img: "/assets/images/professions/4/11.jpg"},
+  {img: "/assets/images/professions/4/12.jpg"},
 ]
 export const dataSponsors_h_3 = getSliderElements(dataSponsorsSource_h_3)
 
